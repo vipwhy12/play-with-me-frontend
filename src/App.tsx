f58b5ad42b9961f6419cdc895cfff488c350308e
Yuna <85014628+vipwhy12@users.noreply.github.com>
2024-01-23 09:07:02 +0900

    fix : 채팅방 오류 시, 새로고침 되는 오류 버그 해결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Chats from './pages/chat/Chats';
 import NewChat from './pages/chat/Newchat';
+import Chat from './pages/chat/Chat';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
            */}
           <Route path="/chats" element={<Chats />} />
           <Route path="/newchat" element={<NewChat />} />
+          <Route path="/chat" element={<Chat />} />
         </Routes>
       </Router>
     </div>
